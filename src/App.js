d2c844bc771ae91218b12464f44539911e9bee12
e-haghparast <73380762+e-haghparast@users.noreply.github.com>
2023-01-18 23:40:34 +0330

    created react project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import Search from "./search.js";
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container pt-5">
+      <div className="App border w-50 pt-3">
+        <Search />
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/search.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import axios from "axios";
+import css from "./search.css";
+export default function Search() {
+  let [city, setCity] = useState("");
+  let [loaded, setLoaded] = useState(false);
+  let [weather, setWeather] = useState({});
+
+  function displayWeather(response) {
+    setLoaded(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "bfd67b65e01f8c3751ffb4a48f09d863";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayWeather);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input type="search" placeholder="Enter a city.." onChange={updateCity} className=" w-50 p-2 me-2 mb-4 rounded shadow"/>
+      <button type="Submit" className="btn btn-primary submit-button ">
+        Search
+      </button>
+    </form>
+  );
+
+  //   if (loaded) {
+  return (
+    <div className="container">
+      <div className="row">{form}</div>
+      <div className="row">
+        <div className="col-6">
+          <ul className="list-unstyled">
+            <li>Description: {weather.description}</li>
+            <li>Humidity: {weather.humidity}%</li>
+            <li className="fs-1">{Math.round(weather.temperature)}°C</li>
+          </ul>
+        </div>
+        <div className="col-6">
+          <ul className="list-unstyled">
+            <li>Wind: {weather.wind}km/h</li>
+            <li>
+              <img src={weather.icon} alt={weather.description} />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+  //   } else {
+  //     return form;
+  //   }
+}
