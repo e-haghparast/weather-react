c8d9576ea175d340ab2cace2e2820fbfa1eb1111
e-haghparast <73380762+e-haghparast@users.noreply.github.com>
2023-01-20 23:56:46 +0330

    fix build error

--- src/search.js
+++ src/search.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import "./search.css";
 export default function Search() {
